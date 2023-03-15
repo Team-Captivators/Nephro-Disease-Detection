36b6cf4adc2d2bbea2c53ca816bdf074b709160c
PRMARAMBE <pasindu.20210375@iit.ac.lk>
2023-03-15 16:00:49 +0530

    Update UI

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,48 @@
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 import "./Contact.css";
 
-function Contact(){
+const Result = () => {
+    return(
+        <p>Your message has been successfully sent. I will contact you soon.</p>
+    );
+};
+
+const Contact = () => {
+    const form = useRef();
+    const [result, showResult] = useState(false);
+    
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_4vuzzqz', 'template_w9qgsqb', form.current, 'Grh6lQkAe9GBU3nU1')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset();
+        showResult(true);
+    };
+
+    // Hide result
+    setTimeout(() => {
+        showResult(false)
+    },5000);
+
     return(
         <div className="form-container">
             <h1>CONTACT US</h1>
             <h3>Email us, we would love to hear form you</h3>
-            <form action="mailto:nephroai@gmail.com" method="post" enctype="text/plain">
-                <input type="text" placeholder="Name" required/>
-                <input type="email" placeholder="Email" required/>
-                <textarea placeholder="Message" rows="4"></textarea>
-                <button >Send Message</button>
+            <form ref={form} onSubmit={sendEmail}>
+            <input type="text" name="user_name" placeholder="Name" required/>
+            <input type="email" name="user_email" placeholder="Email" required/>
+            <textarea name="message" placeholder="Message" rows="4" required></textarea>
+            <button role="button"><span class="text">Send Message</span></button>
+            <div className="row">{result ? <Result /> : null}</div>
             </form>
         </div>
     )
-}
+};
 
 export default Contact; 
