bdaf8732d7acf86175825e62ec377ed2a435ee64
PRMARAMBE <pasindu.20210375@iit.ac.lk>
2023-03-18 00:21:50 +0530

    Update Stage Page

--- src/components/StageData.js
+++ src/components/StageData.js
@@ -2,13 +2,7 @@ import "./Stage.css";
 
 function StageData(props){
     return(
-        // <div className="card">
-        //     <h4><i className={props.icon}></i>{props.mainheading}</h4>
-        //     <h1>{props.heading}</h1>
-        //     <p>{props.text1}</p>
-        //     <p>{props.text2}</p>
-        // </div>
-        <div className="card">
+        <div className="stagedata-card">
             <h4><i className={props.icon}></i>{props.mainheading}</h4>
             <h1>{props.heading}</h1>
             <p>{props.text1}</p>
