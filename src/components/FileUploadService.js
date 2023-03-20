57acd8fad3058d03032e7b7bea9d3560b2a6abe1
josephjerrickgodwin <jerrick.20211156@iit.ac.lk>
2023-03-20 22:28:53 +0530

    Minor update to UI

--- src/components/FileUploadService.js
+++ src/components/FileUploadService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api_url = "http://192.168.1.71:7071/api/nephro_ai" // "https://nephroapi.azurewebsites.net/api/nephro_ai";
+const api_url = "https://nephroapi.azurewebsites.net/api/nephro_ai";
 const upload = (base64String, onUploadProgress) => {
 
     const data = JSON.stringify({
@@ -9,7 +9,6 @@ const upload = (base64String, onUploadProgress) => {
 
     return axios.post(api_url, data, {
         headers: {
-            "Access-Control-Allow-Origin": "*",
             'Content-Type': 'application/json'
         },
         onUploadProgress,
